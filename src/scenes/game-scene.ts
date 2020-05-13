cdb25d47f5218fe3fa4b59d91bb7b2d82b7a6c53
jasontan <jasontan056@gmail.com>
2020-05-12 23:07:50 -0700

    Animate goblin walking

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -8,43 +8,28 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 export class GameScene extends Phaser.Scene {
-
-  public speed: number = 200;
-
-  private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
-  private image: Phaser.Physics.Arcade.Sprite;
-
   constructor() {
     super(sceneConfig);
   }
 
+  public preload() {
+    this.load.spritesheet('goblin', 'assets/goblin_run_spritesheet.png', { frameWidth: 16, frameHeight: 16 });
+  }
+
   public create() {
-    // Add a player sprite that can be moved around. Place him in the middle of the screen.
-    this.image = this.physics.add.sprite(getGameWidth(this) / 2, getGameHeight(this) / 2, 'man');
+    const goblin = this.add.sprite(400, 300, 'goblin').setScale(2);
+    goblin.setOrigin(.5);
+    const goblinWalk = this.anims.create({
+      key: 'walk',
+      frames: this.anims.generateFrameNumbers('goblin', { start: 0, end: 5 }),
+      frameRate: 10,
+      repeat: -1
+    })
+    goblin.anims.load('walk');
+    goblin.anims.play('walk');
 
-    // This is a nice helper Phaser provides to create listeners for some of the most common keys.
-    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    //goblin.scaleX *= -1;
   }
 
-  public update() {
-    // Every frame, we create a new velocity for the sprite based on what keys the player is holding down.
-    const velocity = new Phaser.Math.Vector2(0, 0);
-
-    if (this.cursorKeys.left.isDown) {
-      velocity.x -= 1;
-    }
-    if (this.cursorKeys.right.isDown) {
-      velocity.x += 1;
-    }
-    if (this.cursorKeys.up.isDown) {
-      velocity.y -= 1;
-    }
-    if (this.cursorKeys.down.isDown) {
-      velocity.y += 1;
-    }
-
-    // We normalize the velocity so that the player is always moving at the same speed, regardless of direction.
-    const normalizedVelocity = velocity.normalize();
-    this.image.setVelocity(normalizedVelocity.x * this.speed, normalizedVelocity.y * this.speed);
-  }
+  public update() {}
 }
