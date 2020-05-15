c704f568c768a86a151252d55cb5acff7d1590d9
jasontan056 <jasontan056@gmail.com>
2020-05-15 11:42:29 -0700

    Hold down mouse to add goblins

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,35 +1,46 @@
-import { Input } from 'phaser';
-import { getGameWidth, getGameHeight } from '../helpers';
+import { Input } from "phaser";
+import { getGameWidth, getGameHeight } from "../helpers";
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
   visible: false,
-  key: 'Game',
+  key: "Game",
 };
 
 export class GameScene extends Phaser.Scene {
+  private goblins = [];
+
   constructor() {
     super(sceneConfig);
   }
 
   public preload() {
-    this.load.spritesheet('goblin', 'assets/goblin_run_spritesheet.png', { frameWidth: 16, frameHeight: 16 });
+    this.load.spritesheet("goblin", "assets/goblin_run_spritesheet.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
   }
 
   public create() {
-    const goblin = this.add.sprite(400, 300, 'goblin').setScale(2);
-    goblin.setOrigin(.5);
-    const goblinWalk = this.anims.create({
-      key: 'walk',
-      frames: this.anims.generateFrameNumbers('goblin', { start: 0, end: 5 }),
+    // Walking animation.
+    this.anims.create({
+      key: "walk",
+      frames: this.anims.generateFrameNumbers("goblin", { start: 0, end: 5 }),
       frameRate: 10,
-      repeat: -1
-    })
-    goblin.anims.load('walk');
-    goblin.anims.play('walk');
-
-    //goblin.scaleX *= -1;
+      repeat: -1,
+    });
   }
 
-  public update() {}
+  public update() {
+    const pointer = this.input.activePointer;
+    if (pointer.isDown) {
+      const x = pointer.x;
+      const y = pointer.y;
+
+      const goblin = this.add.sprite(x, y, "goblin");
+      goblin.anims.load("walk");
+      goblin.anims.play("walk");
+      this.goblins.push(goblin);
+    }
+  }
 }
